d643013d9d32f9525cb90abd2431e0980f8ff8f7
tcloaa <zixiang_luo@163.com>
2017-05-26 14:18:08 +0800

    finish data preprocessing for net_change

--- autoencoder.py
+++ autoencoder.py
@@ -6,6 +6,8 @@ Created on Thu May 25 16:12:35 2017
 @author: phil
 """
 
+#%%
+
 import numpy as np
 import pandas as pd
 
@@ -16,17 +18,21 @@ from keras.optimizers import Adam
 import matplotlib.pyplot as plt
 
 
-#%%
 ## load data
-
 print("Loading the data ...")
-data = pd.read_csv('full_data/net_change.csv', header=0).dropna(axis=1, how='any')
+full_data = pd.read_csv('full_data/net_change.csv', header=0).T # inlcude date
 
-date_map = data.iloc[:,0] #index -> date
+data = full_data.drop(["Unnamed: 0"]).dropna(axis=0, how='any') # axis=0: x-axis
+
+train_data = data.iloc[0:, 0:104] # 2012.01 ~ 2013.12
+test_data = data.iloc[0:, 104:226] # 2014.01 ~ 2016.04
 
 
-train_data = data.iloc[0:104, 1:] # 2012.01 ~ 2013.12
-test_data = data.iloc[104:226, 1:] # 2014.01 ~ 2016.04
+'''
+for train_data, test_data
+
+    row: stock
+    column: data
+'''
 #%%
 
-## Auto Encoder
