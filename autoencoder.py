ff202304a601a1861929c86efb2ca49a99f2c4c8
tcloaa <zixiang_luo@163.com>
2017-05-27 16:59:45 +0800

    add stock communal information
    ignore last commit

--- autoencoder.py
+++ autoencoder.py
@@ -72,27 +72,30 @@ decoded_imgs = decoder.predict(encoded_imgs)
 communal_information = []
 
 for i in range(0,83):
+    
     '''
     stock_2norm = np.linalg.norm(train_np[i,:])
     auto_2norm = np.linalg.norm(decoded_imgs[i,:])
     communal_information.append(float(abs(stock_2norm - auto_2norm)))
     '''
+
     difference = np.linalg.norm((train_np[i,:]-decoded_imgs[i,:]))
     communal_information.append(float(difference))
  
 #%%     
 
 '''
-AMGN: should be the highest communal stock
-BCRX: should be the lowest commnual stock
+AMGN: smallest 2-norm diff -> should be the highest communal stock
+BCRX: largest 2-norm diff -> should be the lowest commnual stock
+
 
 10 most communal stocks + x most non-communal stocks
 '''
 
 stock_to_rank = np.array(communal_information).argsort()
 
-for i in stock_to_rank:
-    print(train_data.iloc[i,:].name) #print stock name from lowest to highest
+for row_number in stock_to_rank:
+    print(row_number, communal_information[row_number], train_data.iloc[row_number,:].name) #print stock name from lowest to highest
 
 
 
