817a57fa65f9a743773a5d54833d9fe2e9f1ee1d
tcloaa <zixiang_luo@163.com>
2017-05-27 20:57:32 +0800

    finish auto-encoder part
    up to plot 1 chosen stock

--- autoencoder.py
+++ autoencoder.py
@@ -40,10 +40,10 @@ for test_data
 
 encoding_dim = 5 # 5 neurons
 
+# autoencoder model
 input_img = Input(shape=(104,))
-
 encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(input_img)
-decoded = Dense(104, activation='relu',activity_regularizer=regularizers.l1(10e-5))(encoded)
+decoded = Dense(104, activation='linear', activity_regularizer=regularizers.l1(10e-5))(encoded) # see 'Stacked Auto-Encoders' in paper
 autoencoder = Model(input_img, decoded)
 
 # predict model
@@ -54,36 +54,22 @@ decoder = Model(encoded_input, decoder_layer(encoded_input))
 
 
 autoencoder.compile(loss='mean_squared_error', optimizer='sgd')
-
+#%%
 
 # train autoencoder
 train_np = train_data.as_matrix()
-autoencoder.fit(train_np, train_np, epochs=1000)
+autoencoder.fit(train_np, train_np, shuffle=True, epochs=5000)
 
-#%%
 
-# predict result
+'''
 encoded_imgs = encoder.predict(train_np)
 decoded_imgs = decoder.predict(encoded_imgs)
+'''
+#%%
+# predict result
+decoded_imgs = autoencoder.predict(train_np)
 
 #%%
-
-# 2 norm difference
-communal_information = []
-
-for i in range(0,83):
-    
-    '''
-    stock_2norm = np.linalg.norm(train_np[i,:])
-    auto_2norm = np.linalg.norm(decoded_imgs[i,:])
-    communal_information.append(float(abs(stock_2norm - auto_2norm)))
-    '''
-
-    difference = np.linalg.norm((train_np[i,:]-decoded_imgs[i,:]))
-    communal_information.append(float(difference))
- 
-#%%     
-
 '''
 AMGN: smallest 2-norm diff -> should be the highest communal stock
 BCRX: largest 2-norm diff -> should be the lowest commnual stock
@@ -92,14 +78,61 @@ BCRX: largest 2-norm diff -> should be the lowest commnual stock
 10 most communal stocks + x most non-communal stocks
 '''
 
-stock_to_rank = np.array(communal_information).argsort()
+# 2 norm difference
+communal_information = []
 
+for i in range(0,83):
+    '''
+    stock_2norm = np.linalg.norm(train_np[i,:])
+    auto_2norm = np.linalg.norm(decoded_imgs[i,:])
+    communal_information.append(float(abs(stock_2norm - auto_2norm)))
+    '''
+    difference = np.linalg.norm((train_np[i,:]-decoded_imgs[i,:]))
+    communal_information.append(float(difference))
+ 
+
+stock_to_rank = np.array(communal_information).argsort()
 for row_number in stock_to_rank:
     print(row_number, communal_information[row_number], train_data.iloc[row_number,:].name) #print stock name from lowest to highest
 
+#%%
+
+'''
+View highest commnual stock (68 1.3588824272155762 TLGT    US Equity) last_price by:
+    
+    1. load last_price data
+    2. plot original last_price
+    3. plot predict last_price
+    
+Also lowest communal stock (4 92.86773681640625 REGN    US Equity)    
+'''
 
 
+full_last_price = pd.read_csv('full_data/last_price.csv', header=0).T # inlcude date
 
+last_price = full_last_price.drop(["Unnamed: 0"]).dropna(axis=0, how='any').astype('float32') # axis=0: x-axis
+
+train_last_price = last_price.iloc[0:, 0:104] # 2012.01 ~ 2013.12
+
+#%%
+which_stock = 38
+
+tlgt_origin = pd.Series(train_last_price.iloc[which_stock,0:].as_matrix(), index=pd.date_range(start='01/06/2012', periods=104, freq='W'))
+tlgt_origin.plot()
+
+# now predict price
+tlgt_autoencoder = []
+price = 0
+for i in range(0,104):
+    if i == 0:
+        price = train_last_price.iloc[which_stock,0]
+    else:
+        price = price + decoded_imgs[which_stock,i]
+    tlgt_autoencoder.append(price)
+
+    
+tlgt_autoencoder = pd.Series(tlgt_autoencoder, index=pd.date_range(start='01/06/2012', periods = 104,freq='W'))
+tlgt_autoencoder.plot()
 
 
 
