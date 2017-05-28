a1a2210794828173bb0261de585a40ade90aca13
tcloaa <zixiang_luo@163.com>
2017-05-28 15:56:47 +0800

    ibb 完美拟合

--- autoencoder.py
+++ autoencoder.py
@@ -19,68 +19,60 @@ from keras import regularizers
 from keras.models import load_model
 
 #%%
+'''
+Load All Data Here
+
+for train_data, test_data
+    83 row: stock
+    104 column: date   
+'''
+## load ibb all 3 frames
+ibb = pd.read_csv('full_data/ibb_uq.csv', header=0).T # date index: 0 ~ 225
+ibb_lp = ibb.iloc[1,:].astype('float32') # all last price
+ibb_lp_train = ibb_lp.iloc[0:104]
+ibb_lp_test = ibb_lp.iloc[104:208]
+
+ibb_net = ibb.iloc[2,:].astype('float32') # all net change
+ibb_net_train = ibb_net.iloc[0:104]
+ibb_net_test = ibb_net.iloc[104:208]
 
 
-## load data
+## load stock last price
+stock_lp = pd.read_csv('full_data/last_price.csv', header=0).T.drop(["Unnamed: 0"]).dropna(axis=0, how='any').astype('float32') # inlcude date
+stock_lp_train = stock_lp.iloc[0:, 0:104] # 2012.01 ~ 2013.12
+stock_lp_test = stock_lp.iloc[0:, 104:208] # 2014.01 ~ 2016.04
+
+
+## load stock net change
 print("Loading the data ...")
-full_data = pd.read_csv('full_data/net_change.csv', header=0).T # inlcude date
-
-data = full_data.drop(["Unnamed: 0"]).dropna(axis=0, how='any').astype('float32') # axis=0: x-axis
-
-train_data = data.iloc[0:, 0:104] # 2012.01 ~ 2013.12
-test_data = data.iloc[0:, 104:226] # 2014.01 ~ 2016.04
+stock_net = pd.read_csv('full_data/net_change.csv', header=0).T.drop(["Unnamed: 0"]).dropna(axis=0, how='any').astype('float32')  # include date,  # axis=0: x-axis
+stock_net_train = stock_net.iloc[0:, 0:104] # 2012.01 ~ 2013.12
+stock_net_test = stock_net.iloc[0:, 104:208] # 2014.01 ~ 2016.04
 
 
-'''
-for train_data
-    83 row: stock
-    104 column: date
-    
-for test_data
-    83 row: stock
-    122 column: date    
-'''
-
+## global variable
 encoding_dim = 5 # 5 neurons
-
-# autoencoder model
-input_img = Input(shape=(104,))
-encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(input_img)
-decoded = Dense(104, activation='linear', activity_regularizer=regularizers.l1(10e-5))(encoded) # see 'Stacked Auto-Encoders' in paper
-autoencoder = Model(input_img, decoded)
-autoencoder.compile(loss='mean_squared_error', optimizer='sgd')
-
-# train autoencoder
-train_np = train_data.as_matrix()
-autoencoder.fit(train_np, train_np, shuffle=True, epochs=5000)
-
-# predict result
-decoded_imgs = autoencoder.predict(train_np)
 #%%
 '''
 AMGN: smallest 2-norm diff -> should be the highest communal stock
 BCRX: largest 2-norm diff -> should be the lowest commnual stock
-
-
 10 most communal stocks + x most non-communal stocks
 '''
 
-# 2 norm difference
+# predict result
+autoencoder = load_model('autoencoder.h5')
+decoded_net_train = autoencoder.predict(stock_net_train.as_matrix())
+
 communal_information = []
 
 for i in range(0,83):
-    '''
-    stock_2norm = np.linalg.norm(train_np[i,:])
-    auto_2norm = np.linalg.norm(decoded_imgs[i,:])
-    communal_information.append(float(abs(stock_2norm - auto_2norm)))
-    '''
-    difference = np.linalg.norm((train_np[i,:]-decoded_imgs[i,:]))
+    difference = np.linalg.norm((stock_net_train.iloc[i,:]-decoded_net_train[i,:])) # 2 norm difference
     communal_information.append(float(difference))
  
 
 stock_to_rank = np.array(communal_information).argsort()
 for row_number in stock_to_rank:
-    print(row_number, communal_information[row_number], train_data.iloc[row_number,:].name) #print stock name from lowest to highest
+    print(row_number, communal_information[row_number], stock_net_train.iloc[row_number,:].name) #print stock name from lowest to highest
 
 #%%
 
@@ -92,64 +84,37 @@ View highest commnual stock (68 1.3588824272155762 TLGT    US Equity) last_price
     3. plot predict last_price
     
 Also lowest communal stock (4 92.86773681640625 REGN    US Equity)    
+
+for 1 chosen stock, plot its original price & autoencoded price for comparison
 '''
 
+which_stock = 1
+# original last price plot
+pd.Series(stock_lp_train.iloc[which_stock,0:].as_matrix(), index=pd.date_range(start='01/06/2012', periods=104, freq='W')).plot(label='stock original', legend=True)
 
-full_last_price = pd.read_csv('full_data/last_price.csv', header=0).T # inlcude date
-
-last_price = full_last_price.drop(["Unnamed: 0"]).dropna(axis=0, how='any').astype('float32') # axis=0: x-axis
-
-train_last_price = last_price.iloc[0:, 0:104] # 2012.01 ~ 2013.12
-
-#%%
-
-'''
-    for 1 chosen stock, plot its original price & autoencoded price for comparison
-'''
-
-which_stock = 76
-
-stock_origin = pd.Series(train_last_price.iloc[which_stock,0:].as_matrix(), index=pd.date_range(start='01/06/2012', periods=104, freq='W'))
-stock_origin.plot(label='stock original', legend=True)
-
-# now predict price
+# now decoded last price plot
 stock_autoencoder = []
 price = 0
 for i in range(0,104):
     if i == 0:
-        price = train_last_price.iloc[which_stock,0]
+        price = stock_lp_train.iloc[which_stock,0]
     else:
-        price = price + decoded_imgs[which_stock,i]
+        price = price + decoded_net_train[which_stock,i]
     stock_autoencoder.append(price)
 
     
-stock_autoencoder = pd.Series(stock_autoencoder, index=pd.date_range(start='01/06/2012', periods = 104,freq='W'))
-stock_autoencoder.plot(label='stock autoencoded', legend=True)
+pd.Series(stock_autoencoder, index=pd.date_range(start='01/06/2012', periods = 104,freq='W')).plot(label='stock auto', legend=True)
 #%%
 
 '''
-Calibration Phase
+Phase 2: Calibration 
     1. load ibb last_price (will do ibb modification in next .py)
     2. plot ibb last_price
     3. infer the weight for S25, S45, S65
     4. plot portfolio price
-
 '''
 
-# Step 1 & 2
-full_ibb = pd.read_csv('full_data/ibb_uq.csv', header=0).T # date index: 0 ~ 225
-ibb_last_price = full_ibb.iloc[1,:].astype('float') # all last price
-ibb_last_train = ibb_last_price.iloc[0:104]
-ibb_last_test = ibb_last_price.iloc[104:226]
-
-ibb_plot_train_origin = pd.Series(np.array(ibb_last_train), index=pd.date_range(start='01/06/2012', periods=104, freq='W'))
-ax= ibb_plot_train_origin.plot()
-ax.set_xlabel('Weekly Date')
-ax.set_ylabel('IBB Last Price')
-
-#%%
-
-# step 3: infer weight
+# Infer weight
 
 '''
 10 most commnual stock + x most non-communal stock
@@ -158,27 +123,36 @@ so S25, S45, S65
 '''
 x = 15
 s = x+10
-most_com_10 = stock_to_rank[0:10]
-least_com_10 = stock_to_rank[-x:]
-select_index = np.concatenate((most_com_10, least_com_10))
-select_stock = train_last_price.iloc[select_index,0:].T # we have S25 here
+port_index = np.concatenate((stock_to_rank[0:10], stock_to_rank[-x:])) # portfolio index
+port_lp_train = stock_net_train.iloc[port_index, 0:].T # we have S25 here
+port_net_train = stock_net_train.iloc[port_index, 0:].T # we have S25 here
 
 
-#%%
 # deep learner model
-portfolio = Input(shape=(s,)) # row: 104 dates, column: S stocks
+portfolio = Input(shape=(s,)) # row: 104 dates, column: 10+x stocks
 learner_hidden = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(portfolio)
-learner_output = Dense(1, activation='relu', activity_regularizer=regularizers.l1(10e-5))(learner_hidden) # output layer
+learner_output = Dense(1, activation='linear', activity_regularizer=regularizers.l1(10e-5))(learner_hidden) # output layer
 deep_learner = Model(portfolio, learner_output)
 deep_learner.compile(loss='mean_squared_error', optimizer='sgd')
 
-# train autoencoder
-deep_learner.fit(select_stock.as_matrix().astype('float'), ibb_last_train.as_matrix().astype('float'), epochs=5000) # ??? f(stock_last) = ibb_last
+deep_learner.fit(port_net_train.as_matrix(), ibb_net_train.as_matrix(), epochs=5000) # ??? f(stock_net) = ibb_net
 
-# predict result
-ibb_learner_train = deep_learner.predict(select_stock.as_matrix())
+# predict deep-learned ibb net change
+ibb_learner_net_train = deep_learner.predict(port_net_train.as_matrix())
 
-
+#%%
+# calculate deep-learned ibb last price
+ibb_autoencoder = []
+price = 0
+for i in range(0,104):
+    if i == 0:
+        price = ibb_lp[0] # 2012.1.6 last price
+    else:
+        price = price + ibb_learner_net_train[i,0]
+    ibb_autoencoder.append(price)
+ 
+pd.Series(ibb_autoencoder, index=pd.date_range(start='01/06/2012', periods = 104,freq='W')).plot(label='ibb auto', legend=True)
+pd.Series(ibb_lp_train.as_matrix(), index=pd.date_range(start='01/06/2012', periods=104, freq='W')).plot(label='ibb origin', legend=True)    
 #%%
 
 '''
