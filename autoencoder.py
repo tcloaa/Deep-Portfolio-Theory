1ed78377631e99d4fe1e5ac9b9b7f28378713469
tcloaa <zixiang_luo@163.com>
2017-05-28 14:55:10 +0800

    back up

--- autoencoder.py
+++ autoencoder.py
@@ -16,8 +16,9 @@ import matplotlib.pyplot as plt
 from keras.layers import Input, Dense
 from keras.models import Model
 from keras import regularizers
+from keras.models import load_model
 
-
+#%%
 
 
 ## load data
@@ -47,30 +48,14 @@ input_img = Input(shape=(104,))
 encoded = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(input_img)
 decoded = Dense(104, activation='linear', activity_regularizer=regularizers.l1(10e-5))(encoded) # see 'Stacked Auto-Encoders' in paper
 autoencoder = Model(input_img, decoded)
-
-# predict model
-encoder = Model(input_img, encoded)# this model maps an input to its encoded representation
-encoded_input = Input(shape=(encoding_dim,))
-decoder_layer = autoencoder.layers[-1]
-decoder = Model(encoded_input, decoder_layer(encoded_input))
-
-
 autoencoder.compile(loss='mean_squared_error', optimizer='sgd')
-#%%
 
 # train autoencoder
 train_np = train_data.as_matrix()
 autoencoder.fit(train_np, train_np, shuffle=True, epochs=5000)
 
-
-'''
-encoded_imgs = encoder.predict(train_np)
-decoded_imgs = decoder.predict(encoded_imgs)
-'''
-#%%
 # predict result
 decoded_imgs = autoencoder.predict(train_np)
-
 #%%
 '''
 AMGN: smallest 2-norm diff -> should be the highest communal stock
@@ -117,10 +102,15 @@ last_price = full_last_price.drop(["Unnamed: 0"]).dropna(axis=0, how='any').asty
 train_last_price = last_price.iloc[0:, 0:104] # 2012.01 ~ 2013.12
 
 #%%
-which_stock = 22
+
+'''
+    for 1 chosen stock, plot its original price & autoencoded price for comparison
+'''
+
+which_stock = 76
 
 stock_origin = pd.Series(train_last_price.iloc[which_stock,0:].as_matrix(), index=pd.date_range(start='01/06/2012', periods=104, freq='W'))
-stock_origin.plot()
+stock_origin.plot(label='stock original', legend=True)
 
 # now predict price
 stock_autoencoder = []
@@ -134,7 +124,7 @@ for i in range(0,104):
 
     
 stock_autoencoder = pd.Series(stock_autoencoder, index=pd.date_range(start='01/06/2012', periods = 104,freq='W'))
-stock_autoencoder.plot()
+stock_autoencoder.plot(label='stock autoencoded', legend=True)
 #%%
 
 '''
@@ -153,13 +143,40 @@ ibb_last_train = ibb_last_price.iloc[0:104]
 ibb_last_test = ibb_last_price.iloc[104:226]
 
 ibb_plot_train_origin = pd.Series(np.array(ibb_last_train), index=pd.date_range(start='01/06/2012', periods=104, freq='W'))
-ibb_plot_train_origin.plot()
+ax= ibb_plot_train_origin.plot()
+ax.set_xlabel('Weekly Date')
+ax.set_ylabel('IBB Last Price')
 
 #%%
 
+# step 3: infer weight
+
+'''
+10 most commnual stock + x most non-communal stock
+Here x = 15, 35, 55, 
+so S25, S45, S65
+'''
+x = 15
+s = x+10
+most_com_10 = stock_to_rank[0:10]
+least_com_10 = stock_to_rank[-x:]
+select_index = np.concatenate((most_com_10, least_com_10))
+select_stock = train_last_price.iloc[select_index,0:].T # we have S25 here
 
 
+#%%
+# deep learner model
+portfolio = Input(shape=(s,)) # row: 104 dates, column: S stocks
+learner_hidden = Dense(encoding_dim, activation='relu', activity_regularizer=regularizers.l1(10e-5))(portfolio)
+learner_output = Dense(1, activation='relu', activity_regularizer=regularizers.l1(10e-5))(learner_hidden) # output layer
+deep_learner = Model(portfolio, learner_output)
+deep_learner.compile(loss='mean_squared_error', optimizer='sgd')
 
+# train autoencoder
+deep_learner.fit(select_stock.as_matrix().astype('float'), ibb_last_train.as_matrix().astype('float'), epochs=5000) # ??? f(stock_last) = ibb_last
+
+# predict result
+ibb_learner_train = deep_learner.predict(select_stock.as_matrix())
 
 
 #%%
@@ -171,6 +188,14 @@ Validation Phase
     3. use infered weight to re-track ibb index
     3. plot portfolio price
 '''
+
+ibb_plot_test_origin = pd.Series(np.array(ibb_last_test), index=pd.date_range(start='01/06/2013', periods=104, freq='W'))
+ax= ibb_plot_train_origin.plot()
+ax.set_xlabel('Weekly Date')
+ax.set_ylabel('IBB Last Price')
+
+
+#ibb_learner_test = deep_learner.predict() # plug in S25 test date data
 
 #%%
 
